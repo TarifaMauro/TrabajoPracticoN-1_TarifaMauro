f2c51169d190ab352baa940e13dc71dfdbde0efa
DaSta <DaSta@DESKTOP-AB6GO6R>
2024-04-09 02:23:05 -0300

    se agrega ejercicio 2 completo

---
+++ tp1_TarifaMauroAgustín/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -0,0 +1,23 @@
+package ar.edu.unju.fi.ejercicio2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String nombrePais = "Argentina";
+		byte edadPersna = 26;
+		short alturaEdificio = 90;
+		float precio = 1999.99f;
+		String numTelefono = "+543883457689";
+		Double coseno = Math.cos(0.5);
+		
+		System.out.println("Pais: "+nombrePais);
+		System.out.println("Edad de una persona: " +edadPersna+" años");
+		System.out.println("ALtura de un edificio: " +alturaEdificio+"m");
+		System.out.println("Precio de un producto de super mercado: " +precio+"$");
+		System.out.println("Un numero de telefono: " +numTelefono);
+		System.out.println("El coseno de 0.5 es: " +coseno);
+
+
+	}
+
+}
