1aa3e82f29cc855c2439b5f1955e054d1c993bc2
DaSta <DaSta@DESKTOP-AB6GO6R>
2024-04-09 02:17:11 -0300

    Se agrega ejercicio 1 realizado

---
+++ tp1_TarifaMauroAgustín/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,12 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int num1 = 8, num2 = 5, num3 = 1, num4 = 20, num5 = 9;
+		float sum = num1+num2+num3+num4+num5;
+		float prom = sum/5;
+		System.out.println("El promedio de la suma es: " +prom);
+	}
+
+}
